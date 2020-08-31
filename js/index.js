c22c15fc29f8401af1267a833c259544bd1242c2
JohnC27 <evgeniy89evgeniy89@gmail.com>
2020-08-31 15:13:52 +0500

    added intersectionsObserver

---
+++ js/index.js
@@ -0,0 +1,26 @@
+window.onload = () => {
+  const options = {
+    root: null,
+    rootMargin: `0px`,
+    threshold: 0.2
+  };
+
+  const observer = new IntersectionObserver((entries, observer) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        const block = entry.target;
+
+        block.querySelector(`.info__background`).classList.add(`info__background--blur`);
+        block.querySelector(`.info__content`).classList.add(`info__content--visible`);
+
+        observer.unobserve(entry.target);
+      }
+    });
+  }, options);
+
+  const infoBlocks = document.querySelectorAll(`.info`);
+
+  infoBlocks.forEach((block) => {
+    observer.observe(block);
+  });
+}
